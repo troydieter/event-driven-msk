5a59a5dfb8e3eda9ba77386e4448fef3ea5fecd2
Troy Dieter <troydieter@gmail.com>
2021-11-28 13:43:33 -0500

    dropped remote_state.tf

--- remote_state.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket               = "troydieter.com-tfstate"
-    key                  = "event-driven-msk.tfstate"
-    workspace_key_prefix = "event-driven-msk-tfstate"
-    region               = "us-east-1"
-    dynamodb_table       = "td-tf-lockstate"
-  }
-}
