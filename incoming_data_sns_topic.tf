6be261fd5aa8526d5d465919b1e00ffcb51473c7
Troy Dieter <troydieter@gmail.com>
2021-11-27 21:07:33 -0500

    re-add fifo to sns

--- incoming_data_sns_topic.tf
+++ incoming_data_sns_topic.tf
@@ -6,11 +6,8 @@ module "sns_encrypted_incoming_data" {
   name_prefix       = "incoming-data-sns-${random_id.rando.hex}-"
   display_name      = "incoming-data-sns-${random_id.rando.hex}"
   kms_master_key_id = aws_kms_key.incoming_data_kms_key.id
-  ############################################################
-  # Lambda subscriptions to SNS topics only supports standard topics, not FIFO yet
-  # fifo_topic = true
-  # content_based_deduplication = true
-  ############################################################
+  fifo_topic = true
+  content_based_deduplication = true
 
   tags = {
     "project"     = "${lower("${var.aws-profile}")}-event-driven-msk"
