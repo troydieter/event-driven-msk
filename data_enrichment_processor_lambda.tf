c5dbccfa87ec7f6e86f88ceaa618c25b38101793
Troy Dieter <troydieter@gmail.com>
2021-11-28 00:02:02 -0500

    maybe more bug bustin`

--- data_enrichment_processor_lambda.tf
+++ data_enrichment_processor_lambda.tf
@@ -9,6 +9,24 @@ module "data_platform_lambda_function" {
   runtime       = "python3.8"
 
   source_path = "./src/msk-processor-lambda"
+    attach_policy_json = true
+  policy_json        = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+              "kafka:*"
+            ],
+            "Resource": [
+              "${aws_msk_cluster.data_platform.arn}",
+              "${aws_msk_cluster.data_platform.arn}/*"
+              ]
+        }
+    ]
+}
+EOF
 
   tags = {
     "project"     = "${lower("${var.aws-profile}")}-event-driven-msk"
