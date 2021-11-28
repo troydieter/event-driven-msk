b79ad48bd775b951a7cc1b4c0ed399f5ee81d273
Troy Dieter <troydieter@gmail.com>
2021-11-28 00:08:03 -0500

    more bug bustin`

--- data_publish_event_mapping.tf
+++ data_publish_event_mapping.tf
@@ -3,6 +3,6 @@
 resource "aws_lambda_event_source_mapping" "data_publish_event_mapping" {
   event_source_arn  = module.sqs_encrypted_data_enrichment.sqs_queue_arn
   function_name     = "data_publish-processor-${var.environment}-${random_id.rando.hex}"
-  topics            = var.platform_topic
-  starting_position = "LATEST"
+  #topics            = var.platform_topic
+  #starting_position = "LATEST"
 }
