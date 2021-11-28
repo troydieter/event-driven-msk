59f93a3588f5f1a8549ccc09d0c649683c6529d6
Troy Dieter <troydieter@gmail.com>
2021-11-27 20:23:29 -0500

    fix to name

--- sns_processor_event_sub.tf
+++ sns_processor_event_sub.tf
@@ -9,7 +9,7 @@ resource "aws_sns_topic_subscription" "sns_processor_sub" {
 resource "aws_lambda_permission" "allow_sns_invoke" {
   statement_id  = "AllowExecutionFromSNS"
   action        = "lambda:InvokeFunction"
-  function_name = module.lambda_function.function_name
+  function_name = module.lambda_function.lambda_function_name
   principal     = "sns.amazonaws.com"
   source_arn    = module.sns_encrypted_incoming_data.sns_topic_arn
 }
