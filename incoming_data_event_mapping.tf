0907a00fa34007cfee6fc458752c85cdf2878f7c
Troy Dieter <troydieter@gmail.com>
2021-12-07 02:31:46 -0500

    fixes for sns policy

--- incoming_data_event_mapping.tf
+++ incoming_data_event_mapping.tf
@@ -3,7 +3,7 @@
 resource "aws_sns_topic_subscription" "sns_processor_sub" {
   topic_arn = module.sns_encrypted_incoming_data.sns_topic_arn
   protocol  = "sqs"
-  endpoint  = module.sqs_encrypted_incoming_data.sqs_queue_arn
+  endpoint  = module.incoming_data_lambda_function.lambda_function_arn
 }
 
 resource "aws_lambda_event_source_mapping" "sqs_processor_lambda_event_mapping" {
@@ -31,4 +31,29 @@ data "aws_iam_policy" "iam_msk_lambda_access" {
 resource "aws_iam_role_policy_attachment" "iam_msk_lambda_access-policy-attach" {
   role       = module.incoming_data_lambda_function.lambda_role_name
   policy_arn = data.aws_iam_policy.iam_msk_lambda_access.arn
+}
+
+resource "aws_sqs_queue_policy" "sqs_encrypted_incoming_data" {
+    queue_url = module.sqs_encrypted_incoming_data.sqs_queue_id
+
+    policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "${random_id.rando.hex}-sqs_encrypted_incoming_data",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${module.sqs_encrypted_incoming_data.sqs_queue_arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${module.sns_encrypted_incoming_data.sns_topic_arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
 }
