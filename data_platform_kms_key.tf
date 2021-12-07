d1d82758dab83bf241798be5eeb24e0ec4410a82
Troy Dieter <troydieter@gmail.com>
2021-12-07 01:56:40 -0500

    fixed kms policy issues

--- data_platform_kms_key.tf
+++ data_platform_kms_key.tf
@@ -1,7 +1,35 @@
 # Provisions the KMS key for MSK CloudWatch data
 
 resource "aws_kms_key" "data_platform_kms_key" {
-  description = "Data Platform KMS Key"
+  description = "${random_id.rando.hex}-data_platform_kms_key"
   enable_key_rotation = true
+  policy = <<EOF
+{
+  "Version" : "2012-10-17",
+  "Id" : "${random_id.rando.hex}-data_platform_kms_key",
+  "Statement" : [ {
+      "Sid" : "Enable IAM User Permissions",
+      "Effect" : "Allow",
+      "Principal" : {
+        "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      },
+      "Action" : "kms:*",
+      "Resource" : "*"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": { "Service": "logs.${var.aws_region}.amazonaws.com" },
+      "Action": [ 
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*"
+      ],
+      "Resource": "*"
+    }  
+  ]
+}
+EOF
   tags        = local.common-tags
 }
--- incoming_data_kms_key.tf
+++ incoming_data_kms_key.tf
@@ -3,5 +3,33 @@
 resource "aws_kms_key" "incoming_data_kms_key" {
   description = "Incoming data encryption key"
   enable_key_rotation = true
+  policy = <<EOF
+{
+  "Version" : "2012-10-17",
+  "Id" : "${random_id.rando.hex}-incoming_data_kms_key",
+  "Statement" : [ {
+      "Sid" : "Enable IAM User Permissions",
+      "Effect" : "Allow",
+      "Principal" : {
+        "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      },
+      "Action" : "kms:*",
+      "Resource" : "*"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": { "Service": "logs.${var.aws_region}.amazonaws.com" },
+      "Action": [ 
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*"
+      ],
+      "Resource": "*"
+    }  
+  ]
+}
+EOF
   tags        = local.common-tags
 }
