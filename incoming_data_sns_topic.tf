413a9c30e5183eed8609eab74dceaaa352b86183
Troy Dieter <troydieter@gmail.com>
2022-01-26 20:30:52 -0500

    added dedicated role and feedback

--- incoming_data_sns_topic.tf
+++ incoming_data_sns_topic.tf
@@ -1,12 +1,63 @@
+# Provisions the IAM roles and policies for feedback in CloudWatch
+resource "aws_iam_role" "sns_topic_role" {
+  name = "sns_topic_role-${random_id.rando.hex}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "sns.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = local.common-tags
+}
+
+resource "aws_iam_role_policy" "sns_topic_feedback" {
+  name = "sns_feedback_policy-${random_id.rando.hex}"
+  role = aws_iam_role.sns_topic_role.arn
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:PutMetricFilter",
+          "logs:PutRetentionPolicy"
+        ],
+        "Resource" : [
+          "*"
+        ]
+      }
+    ]
+  })
+
+  tags = local.common-tags
+}
+
 # Provisions the inbound data Amazon SNS Topic
 
 module "sns_encrypted_incoming_data" {
   source = "terraform-aws-modules/sns/aws"
 
-  name_prefix                 = "incoming-data-sns-${random_id.rando.hex}-"
-  display_name                = "incoming-data-sns-${random_id.rando.hex}"
-  kms_master_key_id           = aws_kms_key.incoming_data_kms_key.id
-  fifo_topic                  = true
-  content_based_deduplication = true
-  tags                        = local.common-tags
+  name_prefix                      = "incoming-data-sns-${random_id.rando.hex}-"
+  display_name                     = "incoming-data-sns-${random_id.rando.hex}"
+  kms_master_key_id                = aws_kms_key.incoming_data_kms_key.id
+  fifo_topic                       = true
+  content_based_deduplication      = true
+  sqs_success_feedback_role_arn    = aws_iam_role.sns_topic_role.arn
+  sqs_failure_feedback_role_arn    = aws_iam_role.sns_topic_role.arn
+  sqs_success_feedback_sample_rate = "100"
+
+  tags = local.common-tags
 }
